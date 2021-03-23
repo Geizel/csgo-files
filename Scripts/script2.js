0b029994018d0ee0eb03e67ef06c1adcca77c8ea
Geizel <geiselaj1@gmail.com>
2021-03-23 11:31:13 -0500

    log in js

--- Scripts/script2.js
+++ Scripts/script2.js
@@ -10,3 +10,18 @@ window.addEventListener("mouseup", function(hide){
         dropc.style.display = 'none';
     }
 });
+
+function onSignIn(googleUser) {
+    alert("cat");
+    var profile = googleUser.getBasicProfile();
+    getElementById("sign-in").style.display = "none";
+    profile.getName();
+    profile.getImageUrl();
+  }
+
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+    });
+  }
